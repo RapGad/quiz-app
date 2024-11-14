5489e9f19cc28c866affbbcefe7c6183c57f7a02
rapgad <jeffreyhayford98@gamil.com>
2024-11-14 11:01:52 +0000

    Done another part of the JS

--- quiz.js
+++ quiz.js
@@ -2,19 +2,13 @@ const selectedCategory = localStorage.getItem('category')
 const selectedDifficulty = localStorage.getItem('difficulty')
 const url = `https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=${selectedDifficulty}&type=multiple`
 const questionsContainer = document.querySelector('.questionsContainer')
+const nextQuestionButton = document.getElementById("nextQuestion")
 
 let questionNumber = 0
 let questions = null
 
 
 let progress = 0;
-
-function increaseProgress() {
-    if (progress < 100) {
-        progress += 10; // Increase by 10% each time
-        document.getElementById("progress-bar").style.width = progress + "%";
-    }
-}
 
 
 const getQuestions = async()=>{
@@ -34,23 +28,57 @@ const getQuestions = async()=>{
 }
 
 
-async function createQuestionTag(){
+
+async function createQuestionTag(number){
     const loadedQuestions = await getQuestions()
+    let correctAnswer = Math.floor(Math.random()*3) 
+    let ans
     
-    let eachQuestion = loadedQuestions[questionNumber].map((item,index)=>{
+    let eachQuestion = loadedQuestions[number].map((item)=>{
+        ans = item.correctAnswer
+
         return `
         <p class="question">
-                ${questionNumber + 1}. ${item.question}
-            </p>
+            ${questionNumber + 1}. ${item.question}
+        </p>
+         <ul class="answers">
 
-            <ul class="answers">
-                <li>Sarkodie</li>
-                <li>Manifest</li>
-                <li>Flowking Stone</li>
-                <li>Amerado</li>
-            </ul>
+    ${item.incorrectAnswers.map((answer,index)=>{
+        index === correctAnswer ? ` <li id=${index}>${ans}</li>`:`
+        <li class=answer id=${index}>${answer}</li>`
+  
+    })}
+          </ul>
         `
     })
 
+    questionsContainer.innerHTML = eachQuestion
+
+
+    const answers = document.querySelectorAll(".answer")
+
+    answers.forEach(answer=>{
+        answer.addEventListener('click',(e)=>{
+            if(e.target.id == correctAnswer){
+                answer.classList.add = 'correct'
+            }
+            else{
+                answer.classList.add ='wrong'
+            }
+            answer == correctAnswer ? answer.classList.add = 'correct': answer.classList.add = 'dim'
+        })
+    })
+
 }
-createQuestionTag()
+createQuestionTag(questionNumber)
+
+nextQuestionButton.addEventListener("click",()=>{
+    questionNumber ++
+    createQuestionTag(questionNumber)
+    if (progress < 100) {
+        progress += 10; // Increase by 10% each time
+        document.getElementById("progress-bar").style.width = progress + "%";
+    }
+    
+})
+
