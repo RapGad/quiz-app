342d3832d5b0995ecfb52ee9943ae4bb607f3c2d
rapgad <jeffreyhayford98@gamil.com>
2024-11-18 23:19:35 +0000

    Created a new branch and implemented the stage logic

--- quiz.js
+++ quiz.js
@@ -22,7 +22,9 @@ function decodeHtmlEntities(encodedStr) {
 let questionNumber = 0
 
 let progress = 10;
-function prompt(/*score,level*/){
+function prompt(score,lev,cate){
+    const category = cate
+    const level = lev
     const divContainer = document.createElement('div')
     divContainer.setAttribute('class','container')
     const paraEl = document.createElement('p')
@@ -31,11 +33,53 @@ function prompt(/*score,level*/){
     const replayButton = document.createElement('button')
     const nextButton = document.createElement('button')
 
+    Number(score) < 5 ? nextButton.disabled = true : null
+
+    Number(score) >4 ? lev != 'hard' ?
+    paraEl.textContent = `
+    You Scored ${score} out of 10 <br>
+    Do you wish to move to the next difficulty level or replay??`: `
+    You Scored ${score} out of 10 <br>
+    Yeeeyyyyy You beat Our last level
+    `: `You Scored ${score} out of 10 <br>
+    Do you wish to replay`
+
+
+    if (nextButton.disabled === false) {
+        switch(level){
+        case 'easy':
+            level = 'medium'
+            break
+
+        case 'medium':
+            level = 'hard'
+            break
+
+        default:
+            level = 'easy'
+    }
+}
+
+
+
+
+
+    //The innner functions cannot take parameters from the outer functions
+    //I have to create a variable in the outer function
+    //And set that variable to to the parameters in the inner functions...
     replayButton.addEventListener('click',()=>{
         questionNumber = 0
-        createQuestionTag(questionNumber)
+        createQuestionTag(questionNumber,category,level)
 
     })
+
+    nextButton.addEventListener('click',()=>{
+        questionNumber = 0
+        createQuestionTag(questionNumber,category,level)
+    })
+
+
+
     replayButton.textContent = 'replay'
     nextButton.textContent = 'next'
     btnsContainer.append(replayButton,nextButton)
@@ -44,18 +88,18 @@ function prompt(/*score,level*/){
 }
 
 
-const getQuestions = async()=>{
+const getQuestions = async(selectedCategory,selectedDifficulty)=>{
 
     if(questions.length === 0 && !isLoading){
         isLoading = true
         try{
-            const data = await fetch(url)
+            const data = await fetch(`https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=${selectedDifficulty}&type=multiple`)
             questions = await data.json()
-            error = false
             return questions
     
     
         }catch(error){
+            isLoading = false
             console.error(error)
             error = true
     
@@ -67,24 +111,19 @@ const getQuestions = async()=>{
 
 
 
- async function createQuestionTag(number){
-    await getQuestions()
+ async function createQuestionTag(number,category,level){
+    await getQuestions(category,level)
     const loadedQuestions = questions.results
     let correctAnswer = Math.floor(Math.random()*4) 
     let ansValue
 
-    if(isLoading && !error){
+    if(isLoading){
         questionsContainer.innerHTML = '<p style="text-align: center">Loading...</p>'
-        //return
+        return
     }
 
-    if(error){
-        questionsContainer.innerHTML = '<p style="text-align: center">Error Occured</p>'
-    }
-
-        if(loadedQuestions.length > 0 && !error){
+        if(loadedQuestions != undefined && loadedQuestions.length > 0){
             Object.entries(loadedQuestions[number]).map(([key,value])=>{
-            //let ansValue
             if(key == 'question'){
                 let question = document.createElement('p')
                 question.setAttribute('class','question')
@@ -115,7 +154,7 @@ const getQuestions = async()=>{
         })}
 
         else{
-            questionsContainer.innerHTML = `<p style="text-align: center">Error Occured</p>`
+            questionsContainer.innerHTML = `<p style="text-align: center">Error Occured :(</p>`
             return 
         }
 
@@ -151,8 +190,7 @@ nextQuestionButton.addEventListener("click",(e)=>{
     questionNumber ++
     if(questionNumber === 10){
         e.target.disabled = true
-        prompt()
-        //alert("All done")
+        prompt(Number(score.innerText),selectedCategory,selectedDifficulty)
 
     }
     else createQuestionTag(questionNumber)
